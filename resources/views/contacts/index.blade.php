e6cc9f9e00d00f97c3421fabc2de923e15192a6c
Ali <ma.essa.pk@gmail.com>
2017-08-18 11:02:29 +0100

    Add some spacing

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -14,6 +14,7 @@
           </div>            
           <div class="panel-body">
             <table class="table table-striped table-hover">
+            
               <tr>
                 <th>Name</th>
                 <th>Phone</th>
